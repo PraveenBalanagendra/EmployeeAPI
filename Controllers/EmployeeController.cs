7dff338d422e8b53a1aa27089824e8fafd059d48
PraveenBalanagendra <praveen.balanagendra@gmail.com>
2021-08-04 17:45:50 +0530

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EmployeeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(ILogger<EmployeeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Employee
+            {
+                Id = 1,
+                Name = "Praveen Balanagendra",
+                Salary = 2002.31
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public Employee Post(Employee emp)
+        {
+            return emp;
+        }
+    }
+}
