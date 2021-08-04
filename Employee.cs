80dea31b3c618a6cb3368fc52f19c79f6689660d
PraveenBalanagendra <praveen.balanagendra@gmail.com>
2021-08-04 17:42:57 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeAPI
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+    }
+}
